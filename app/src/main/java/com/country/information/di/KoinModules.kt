cd4b33397c15176cf422ce54171a2dadd751f941
neilm30 <neil_m30@yahoo.com>
2020-11-26 00:51:24 +0530

    update koin modules

--- app/src/main/java/com/country/information/di/KoinModules.kt
+++ app/src/main/java/com/country/information/di/KoinModules.kt
@@ -3,12 +3,24 @@ package com.country.information.di
 import com.country.information.networking.ApiRepository
 import com.country.information.networking.CountryInfoEntryPointApi
 import com.country.information.networking.retrofit.CountryInfoApi
+import com.country.information.networking.retrofit.CountryInfoService
+import com.country.information.uiscreens.CountryTextMapper
+import com.country.information.uiscreens.MainViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     //singleton
-    single<CountryInfoEntryPointApi>{ ApiRepository(get()) }
+    single<CountryInfoEntryPointApi> { ApiRepository(get()) }
+
     //viewmodels
+    viewModel { MainViewModel(get(), get()) }
 
     //factory classes
+    factory { ApiRepository(get()) }
+
+    factory { CountryInfoService() }
+
+    factory { CountryTextMapper(androidContext()) }
 }
