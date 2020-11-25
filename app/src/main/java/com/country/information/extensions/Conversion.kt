cfcab5552b4e7c462ae7e66b7e31800c9027c7b5
neilm30 <neil_m30@yahoo.com>
2020-11-26 00:52:08 +0530

    added extension file

--- app/src/main/java/com/country/information/extensions/Conversion.kt
+++ app/src/main/java/com/country/information/extensions/Conversion.kt
@@ -1,2 +1,10 @@
 package com.country.information.extensions
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+fun Context.isConnectedToInternet() = (this.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager)?.let {
+    it.getNetworkCapabilities(it.activeNetwork)?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
+} ?: false
+
